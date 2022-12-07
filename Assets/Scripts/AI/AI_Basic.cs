c55d8ff870647b8768dae262e077c07cfd202910
Minewarpro <hugo.roche13@hotmail.com>
2022-12-08 00:32:18 +0100

    pnjBasic

---
+++ Assets/Scripts/AI/AI_Basic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI_Basic : MonoBehaviour
+{
+
+    [SerializeField] public float speed = 5;
+    private Vector3 _direction;
+    private RaycastHit Hit;
+
+    void Start()
+    {
+        StartCoroutine(RandomWalk());
+    }
+
+    private IEnumerator RandomWalk()
+    {
+        transform.Rotate(Vector3.up * Random.Range(30, 180));
+        
+        yield return new WaitForSeconds(Random.Range(0f, 8f));
+        StartCoroutine(RandomWalk());
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime); 
+
+        if(Physics.Raycast(transform.position, transform.TransformDirection (Vector3.forward), out Hit, 3))
+        {
+            transform.Rotate(Vector3.up * Random.Range(30, 180));
+        }
+    }
+}
